6a9807a3df094a889d9ce95eafd58c902652cf13
pyuser <pyuser@localhost.localdomain>
2020-12-06 14:06:24 -0500

    Removes test print lines

--- parse_number.py
+++ parse_number.py
@@ -8,10 +8,8 @@ test_list = []
 line_number = 1
 
 for line in f:
-    print(f"{line_number}: {line}")
     for word in line.split():
         if word.isdigit():
-            print(f"\t{line_number}: {word}")
             test_list.append(word)
     line_number += 1
 
