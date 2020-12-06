9493d759b2ffddb23c5e804a85213a2c0f3cb41e
pyuser <pyuser@localhost.localdomain>
2020-12-06 14:13:56 -0500

    Adds sort_list function
    Function is used to convert list items to integers than sort them in ascending order.
    Returns sorted list

--- parse_number.py
+++ parse_number.py
@@ -13,9 +13,18 @@ for line in f:
             test_list.append(word)
     line_number += 1
 
+def sort_list(list):
+    int_list = []
+    for i in list:
+        int_list.append(int(i))
+
+    int_list = sorted(int_list)
+    return int_list
 
 test_list = set(test_list)
-print(f"Test List: {test_list}")
+sorted_test_list = sort_list(test_list)
+print(f"Test List (RAW): {test_list}")
+print(f"Test List (SORTED): {sorted_test_list}")
 
 
 #f.close()
