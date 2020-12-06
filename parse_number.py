3b0e588b7ccddaaba1b8e9cf70e0807f3b12f3df
pyuser <pyuser@localhost.localdomain>
2020-12-06 13:25:10 -0500

    Multiple Changes: Created empty list variable Created line number variable Created function to open, read, and close file Created loop to parse through file and print out the end list

---
+++ parse_number.py
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+file = input("Enter logged file to parse\n> ")
+
+f = open(file, 'r')
+
+test_list = []
+line_number = 1
+
+for line in f:
+    print(f"{line_number}: {line}")
+    for word in line.split():
+        if word.isdigit():
+            print(f"{line_number}: {word}")
+            test_list.append(word)
+        line_number += 1
+
+
+test_list = set(test_list)
+print(f"Test List: {test_list}")
+
+
+#f.close()
